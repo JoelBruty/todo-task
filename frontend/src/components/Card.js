24cdc6052cb01bf0d220f385d704c1bc21cf905d
JoelBruty <165796967+JoelBruty@users.noreply.github.com>
2024-05-30 13:00:42 +0100

    Frontend (delete)

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,16 +1,33 @@
+// import { useEffect } from "react"
 import { Link } from "react-router-dom"
+import { deleteTodo } from "../api/deleteTodo"
 
-const Card = ({ todo, deleteHandler }) => {
+// const Card = ({ todo, deleteHandler }) => {
+const Card = ({ todo }) => {
+
+    const deleteHandler = async (e) => {
+        e.preventDefault()
+        // what function will run?
+        console.log(todo._id)
+        let response = await deleteTodo(todo)
+        console.log(response)
+
+        alert("Deleted item")
+    }
+
+    // useEffect(() => {
+
+    // })
 
     return (
         <div>
             <h3>{todo.text}</h3>
             <button onClick={deleteHandler}>Delete</button>
-            <br/>
+            <br />
             <Link
-                to={`/edit/${todo._id}`} 
+                to={`/edit/${todo._id}`}
             >
-            Edit
+                Edit
             </Link>
         </div>
     )
